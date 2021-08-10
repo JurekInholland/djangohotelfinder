3b43d5630d3d1e3e190be1f8f449e0ce827501e5
Jurek Baumann <jurekbaumann@me.com>
2021-08-10 21:19:24 +0200

    Create models

--- hotelfinder/apps/hotelmanagement/models.py
+++ hotelfinder/apps/hotelmanagement/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class Hotel(models.Model):
+    code = models.CharField(max_length=6, unique=True)
+    name = models.CharField(max_length=120)
+    city = models.ForeignKey(City, on_delete=models.deletion.CASCADE)
+
+    def __str__(self):
+        return self.name
